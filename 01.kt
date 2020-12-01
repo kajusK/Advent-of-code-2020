a257ac77a0b30cb4420406a36ef334c5d4ce20d8
Jakub Kaderka <jakub.kaderka@gmail.com>
2020-12-01 18:12:24 +0100

    Solution to 01

---
+++ 01.kt
@@ -0,0 +1,31 @@
+import java.io.File
+
+fun findTwo(nums: List<Int>): Int {
+    for (a in nums) {
+        for (b in nums) {
+            if (a + b == 2020) {
+                return a*b
+            }
+        }
+    }
+    return -1
+}
+
+fun findThree(nums: List<Int>): Int {
+    for (a in nums) {
+        for (b in nums) {
+            for (c in nums) {
+                if (a + b + c == 2020) {
+                    return a*b*c
+                }
+            }
+        }
+    }
+    return -1
+}
+
+fun main(args: Array<String>) {
+    val nums = File("01.txt").readLines().map { it.toInt() }
+    println("Two items: ${findTwo(nums)}")
+    println("Three items: ${findThree(nums)}")
+}
