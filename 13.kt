f23b712315191316bca167ddda93cbb524cc6af0
Jakub Kaderka <jakub.kaderka@gmail.com>
2020-12-13 21:05:41 +0100

    Refactored day 13

--- 13.kt
+++ 13.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-fun inverse(num: Long, modulo: Int) : Long {
+fun inverse(num: Long, modulo: Long) : Long {
     var x = 2L
     val a = num % modulo
 
@@ -16,10 +16,10 @@ fun main() {
     val content = File("inputs/13.txt").readLines()
     val start = content[0].toInt()
     val departures = content[1].split(',').map {
-        if (it == "x") 0 else it.toInt()
+        if (it == "x") 0L else it.toLong()
     }
 
-    val soonest = departures.filter { it != 0 }. map { it to it - start % it }.minBy { it.second }!!
+    val soonest = departures.filter { it != 0L }. map { it to it - start % it }.minBy { it.second }!!
     println("Part 1: ${soonest.first * soonest.second}")
 
 
@@ -29,13 +29,13 @@ fun main() {
      * The equation for each id is: (t+off) mod id = 0
      * This can be simplified to: t mod id = - (off mod id) = id - (off mod id)
      */
-    val offsets = departures.mapIndexed { i, id -> if (id != 0) id to id-(i % id) else 0 to 0 }.filter { it.first != 0 }
-    val Z = offsets.fold(1L) { acc, (id, off) -> acc * id }
+    val offsets = departures.mapIndexed { i, id -> if (id != 0L) id to id-(i % id) else 0L to 0L }.filter { it.first != 0L }
+    val Z = offsets.map { it.first }.reduce { acc, id -> acc * id }
 
     /* Algorithm is taken from http://voho.eu/wiki/cinska-veta-o-zbytcich/ */
     val res = offsets.map { (id, off) ->
         /* Calculate q */
-        val s = offsets.filter { it.first != id }.fold(1L) { acc, i -> acc*i.first }
+        val s = offsets.map { it.first }.filter { it != id }.reduce { acc, i -> acc*i }
         val t = inverse(s, id)
         off to ((s*t) % Z)
     }.map { (off, q) -> off*q }.sum() % Z
