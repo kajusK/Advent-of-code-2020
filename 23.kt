5b8eedc68fb1e8e7605c0600a7d3d5f7b4b6036b
Jakub Kaderka <jakub.kaderka@gmail.com>
2020-12-24 11:17:48 +0100

    Day 23, part2

--- 23.kt
+++ 23.kt
@@ -1,6 +1,83 @@
-fun main() {
-    var cups = "952438716".map { it.toString().toInt() }.toMutableList()
+data class Cup(val id: Int, var next: Cup?, var prev: Cup?)
 
+class Cups(private val start: List<Int>) {
+    private val cupsCount = 1000000
+    private val map = HashMap<Int, Cup>(cupsCount)
+    private var cur: Cup? = null
+    private var max = 0
+    private var min = 0
+
+    init {
+        var prev: Cup? = null
+
+        val initial = start.forEach {
+            val new = Cup(it, null, prev)
+            map[it] = new
+            prev?.next = new
+            prev = new
+        }
+
+        var id = start.max()!!
+        (start.size until cupsCount).forEach {
+            id++
+            val new = Cup(id, null, prev)
+            map[id] = new
+            prev?.next = new
+            prev = new
+        }
+        prev?.next = map[start[0]]
+        map[start[0]]!!.prev = prev
+        cur = map[start[0]]
+        max = id
+        min = start.min()!!
+    }
+
+    private fun nextId(pickup: Cup): Int {
+        val ids = mutableListOf<Int>()
+        var cup = pickup
+
+        (0 until 3).forEach {
+            ids.add(cup.id)
+            cup = cup.next!!
+        }
+
+        var searchId = cur!!.id
+        do {
+            searchId = if (searchId <= min) max else searchId - 1
+        } while(searchId in ids)
+
+        return searchId
+    }
+
+    private fun round() {
+        val pickup = cur!!.next!!
+        val pickupLast = pickup.next!!.next!!
+
+        cur!!.next = pickupLast.next
+        pickupLast.next!!.prev = cur
+
+        val search = map[nextId(pickup)]!!
+        val next = search.next!!
+        search.next = pickup
+        next.prev = pickupLast
+        pickupLast.next = next
+        pickup.prev = search
+
+        cur = cur?.next
+    }
+
+    fun run(rounds: Int): Long {
+        (0 until rounds).forEach {
+            round()
+        }
+
+        val one = map[1]!!
+        return one.next!!.id.toLong() * one.next!!.next!!.id.toLong()
+    }
+}
+
+fun part1(input: List<Int>) {
+    var cups = input.toMutableList()
     var curPos = 0
 
     (0 until 100).forEach {
@@ -22,4 +99,12 @@ fun main() {
     val start = cups.indexOf(1)
     cups = (cups.subList(start, cups.size) + cups.subList(0, start)).toMutableList()
     println("Part 1: ${cups.subList(1, cups.size).joinToString("")}")
+}
+
+fun main() {
+    val input = "952438716".map { it.toString().toInt() }
+    part1(input)
+
+    val cups = Cups(input)
+    println("Part 2: ${cups.run(10000000)}")
 }
